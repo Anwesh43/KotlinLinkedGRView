f5f67705d9bd9b84afbe378448ae8031eb575b90
Anwesh43 <anweshthecool0@gmail.com>
2018-06-18 09:29:52 +0530

    adding LinkedGRView class

--- linkedgrview/src/main/java/com/anwesh/uiprojects/linkedgrview/LinkedGRView.kt
+++ linkedgrview/src/main/java/com/anwesh/uiprojects/linkedgrview/LinkedGRView.kt
@@ -3,3 +3,24 @@ package com.anwesh.uiprojects.linkedgrview
 /**
  * Created by anweshmishra on 18/06/18.
  */
+
+import android.content.Context
+import android.graphics.*
+import android.view.View
+import android.view.MotionEvent
+
+class LinkedGRView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
